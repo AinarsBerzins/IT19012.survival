76f6b1080e7d4510b42f70b8c2b0fa16af70f3b4
Amakatanau <ainars.berzins@elbune.lv>
2022-05-22 17:00:13 +0300

    ship_control_completed

--- Survival/Assets/ShipController.cs
+++ Survival/Assets/ShipController.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class ShipController : MonoBehaviour
 {
-    public float forwardSpeed = 25f, strafeSpeed = 7.5f, hoverSpeed = 5f;
+    public float forwardSpeed = 1000f, strafeSpeed = 7.5f, hoverSpeed = 5f;
     private float activeForwardSpeed, activeStrafeSpeed, activeHoverSpeed;
     private float forwardAcceleration = 2.5f, strafeAcceleration = 2f, hoverAcceleration = 2f;
     public float lookRateSpeed = 90f;
     private Vector2 lookInput, screenCenter, mouseDistance;
-    private float rollInput;
+    private float rollInput, horizontalInput, verticalInput;
     public float rollSpeed = 90f, rollAcceleration = 3.5f;
+
+    private float inputSpeed = 0f;
 
     void Start()
     {
@@ -20,6 +22,8 @@ public class ShipController : MonoBehaviour
 
     void Update()
     {
+        keyListener();
+       
         lookInput.x = Input.mousePosition.x;
         lookInput.y = Input.mousePosition.y;
 
@@ -27,14 +31,24 @@ public class ShipController : MonoBehaviour
         mouseDistance.y = (lookInput.y - screenCenter.y) / screenCenter.y;
 
         rollInput = Mathf.Lerp(rollInput, Input.GetAxisRaw("Roll"), rollAcceleration * Time.deltaTime);
+        horizontalInput = Mathf.Lerp(horizontalInput, Input.GetAxisRaw("Horizontal"), 0.5f * Time.deltaTime);
+        verticalInput = Mathf.Lerp(verticalInput, Input.GetAxisRaw("Vertical"), 0.5f * Time.deltaTime);
 
-        transform.Rotate(-mouseDistance.y * lookRateSpeed * Time.deltaTime, mouseDistance.x * lookRateSpeed * Time.deltaTime, rollInput * rollSpeed * Time.deltaTime, Space.Self);
-
-        activeForwardSpeed = Mathf.Lerp(activeForwardSpeed, Input.GetAxisRaw("Vertical") * forwardSpeed, forwardAcceleration * Time.deltaTime);
+        activeForwardSpeed = Mathf.Lerp(activeForwardSpeed, inputSpeed, forwardAcceleration * Time.deltaTime);
         activeStrafeSpeed = Mathf.Lerp(activeStrafeSpeed, Input.GetAxisRaw("Horizontal") * strafeSpeed, strafeAcceleration * Time.deltaTime);
         activeHoverSpeed = Mathf.Lerp(activeHoverSpeed, Input.GetAxisRaw("Hover") * hoverSpeed, hoverAcceleration * Time.deltaTime);
 
+        transform.Rotate(-verticalInput * lookRateSpeed * Time.deltaTime, horizontalInput * lookRateSpeed * Time.deltaTime, rollInput * rollSpeed * Time.deltaTime, Space.Self);
         transform.position += transform.forward * activeForwardSpeed * Time.deltaTime;
-        transform.position += (transform.right * activeStrafeSpeed * Time.deltaTime) + (transform.up * activeHoverSpeed * Time.deltaTime);
+
+        Debug.Log("time.deltatime: " + Time.deltaTime);
+        Debug.Log("rollInput: " + rollInput);
+    }
+
+    void keyListener(){
+         if (Input.GetKeyDown(KeyCode.Alpha1)) inputSpeed = 5f; //ir kāds labāks veids, kā šo checkot?
+        else if (Input.GetKeyDown(KeyCode.Alpha2)) inputSpeed = 15f;
+        else if (Input.GetKeyDown(KeyCode.Alpha3)) inputSpeed = 30f;
+        else if (Input.GetKeyDown(KeyCode.Space)) inputSpeed = 0f;
     }
 }
